5a5af93d681ac5ca4ce452ae74d60aeb6303fa97
Yury Madyarov <ymadyarov@flexerasoftware.com>
2017-12-11 18:22:24 +1100

    Remove

--- build/temp/development/Test1/sass/config.rb
+++
@@ -1,2 +0,0 @@
-Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
-cache_path = 'C:\Temp\Test1/build/.sass-cache'
--- build/temp/production/Test1/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require C:\Temp\Test1\app.js
